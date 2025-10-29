977bee21a5cfc8c505c7b86a4e8807715b494b4b
hiluka <lucas.kawakami@sptech.school>
2025-10-29 18:08:38 -0300

    arrumando o tratamento

--- src/main/java/sptech/school/Main.java
+++ src/main/java/sptech/school/Main.java
@@ -15,18 +15,18 @@ public class Main {
         List<Mainframe> listaLidoMainframe = new ArrayList<>();
         List<Processo> listaLidoProcesso = new ArrayList<>();
 
-        // 🔹 Baixa e trata os CSVs do bucket RAW direto da AWS
+        //Baixa e trata os CSVs do bucket RAW direto da AWS
         List<String[]> dadosMainframe = ConexaoAws.lerArquivoCsvDoRaw("dados-mainframe.csv");
         List<String[]> dadosProcesso = ConexaoAws.lerArquivoCsvDoRaw("processos.csv");
 
         importarArquivoCSVMaquinaMemoria(dadosMainframe, listaLidoMainframe);
         importarArquivoCSVProcessoMemoria(dadosProcesso, listaLidoProcesso);
 
-        // 🔹 Gera CSV tratado e envia pro bucket TRUSTED
+        //Gera CSV tratado e envia pro bucket TRUSTED
         String csvTratado = gerarCsvTrusted(listaLidoMainframe, listaLidoProcesso);
         ConexaoAws.enviarCsvTrusted("trusted.csv", csvTratado);
 
-        // 🔹 Valida alertas no Synkro
+        //Valida alertas no Synkro
         validarAlerta(listaLidoMainframe, listaLidoProcesso);
     }
 
@@ -232,17 +232,66 @@ public class Main {
 
     public static void importarArquivoCSVProcessoMemoria(List<String[]> dados, List<Processo> listaLidoProcesso) {
         try {
+            SimpleDateFormat dtEntrada = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            SimpleDateFormat dtSaida = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String ultimoTimestamp = "";
             for (int i = 1; i < dados.size(); i++) {
                 String[] registro = dados.get(i);
-                Processo processo = new Processo();
 
                 try {
-                    processo.setTimestamp(registro[0]);
+                    Date date = dtEntrada.parse(registro[0]);
+                    String timestampAtual = dtSaida.format(date);
+                    if (timestampAtual.equals(ultimoTimestamp)) {
+                        continue;
+                    }
+                    ultimoTimestamp = timestampAtual;
+
+                    Processo processo = new Processo();
+                    processo.setTimestamp(timestampAtual);
+                    processo.setTimestamp(dtSaida.format(date));
                     processo.setMacAdress(registro[1]);
                     processo.setIdentificacaoMainframe(registro[2]);
+
+                    //processos
                     processo.setNome1(registro[6]);
                     processo.setCpu1(Double.parseDouble(registro[7].replace(",", ".")));
                     processo.setMem1(Double.parseDouble(registro[8].replace(",", ".")));
+
+                    processo.setNome2(registro[9]);
+                    processo.setCpu2(Double.parseDouble(registro[10].replace(",", ".")));
+                    processo.setMem2(Double.parseDouble(registro[11].replace(",", ".")));
+
+                    processo.setNome3(registro[12]);
+                    processo.setCpu3(Double.parseDouble(registro[13].replace(",", ".")));
+                    processo.setMem3(Double.parseDouble(registro[14].replace(",", ".")));
+
+                    processo.setNome4(registro[15]);
+                    processo.setCpu4(Double.parseDouble(registro[16].replace(",", ".")));
+                    processo.setMem4(Double.parseDouble(registro[17].replace(",", ".")));
+
+                    processo.setNome5(registro[18]);
+                    processo.setCpu5(Double.parseDouble(registro[19].replace(",", ".")));
+                    processo.setMem5(Double.parseDouble(registro[20].replace(",", ".")));
+
+                    processo.setNome6(registro[21]);
+                    processo.setCpu6(Double.parseDouble(registro[22].replace(",", ".")));
+                    processo.setMem6(Double.parseDouble(registro[23].replace(",", ".")));
+
+                    processo.setNome7(registro[24]);
+                    processo.setCpu7(Double.parseDouble(registro[25].replace(",", ".")));
+                    processo.setMem7(Double.parseDouble(registro[26].replace(",", ".")));
+
+                    processo.setNome8(registro[27]);
+                    processo.setCpu8(Double.parseDouble(registro[28].replace(",", ".")));
+                    processo.setMem8(Double.parseDouble(registro[29].replace(",", ".")));
+
+                    processo.setNome9(registro[30]);
+                    processo.setCpu9(Double.parseDouble(registro[31].replace(",", ".")));
+                    processo.setMem9(Double.parseDouble(registro[32].replace(",", ".")));
+
+
+                    //System.out.printf("%1s %16s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s\n",registro[0],registro[1],registro[2],registro[3],registro[4],registro[5],registro[6],registro[7],registro[8],registro[9],registro[10],registro[11],registro[12]);
+
                     listaLidoProcesso.add(processo);
                 } catch (NumberFormatException erro) {
                     System.out.println("Erro import");
@@ -253,17 +302,43 @@ public class Main {
         }
     }
 
-    public static String gerarCsvTrusted(List<Mainframe> listaMainframe, List<Processo> listaProcesso) {
+    public static String gerarCsvTrusted(List<Mainframe> listamainframe, List<Processo> listaprocesso) {
         StringBuilder sb = new StringBuilder();
-        sb.append("macAdress;timestamp;identificao-mainframe;uso_cpu_total_%;uso_ram_total_%;swap_rate_mbs;tempo_cpu_ociosa;cpu_io_wait;uso_disco_total_%;disco_throughput_mbs;disco_iops_total;disco_read_count;disco_write_count;disco_latencia_ms\n");
 
-        for (Mainframe m : listaMainframe) {
-            sb.append(String.format("%s;%s;%s;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%d;%d;%.2f\n",
-                    m.getMacAdress(), m.getTimestamp(), m.getIdentificaoMainframe(),
-                    m.getUsoCpuTotal(), m.getUsoRamTotal(), m.getSwapRateMbs(),
-                    m.getTempoCpuOciosa(), m.getCpuIoWait(), m.getUsoDiscoTotal(),
-                    m.getDiscoThroughputMbs(), m.getDiscoIopsTotal(),
-                    m.getDiscoReadCount(), m.getDiscoWriteCount(), m.getDiscoLatenciaMs()));
+        sb.append("macAdress;timestamp;identificao-mainframe;uso_cpu_total_%;uso_ram_total_%;uso_disco_total_%;disco_throughput_mbs;disco_iops_total;disco_read_count;disco_write_count;disco_latencia_ms;nome1;cpu_%1;mem_%1;nome2;cpu_%2;mem_%2;nome3;cpu_%3;mem_%3;nome4;cpu_%4;mem_%4;nome5;cpu_%5;mem_%5;nome6;cpu_%6;mem_%6;nome7;cpu_%7;mem_%7;nome8;cpu_%8;mem_%8;nome9;cpu_%9;mem_%9;\n");
+
+        for(Processo processo:listaprocesso){
+            for(Mainframe mainframe:listamainframe){
+                System.out.println("");
+
+                if (mainframe.getTimestamp().equals(processo.getTimestamp())){
+
+                    sb.append(String.format(
+                            "%s;%s;%s;%.2f;%.2f;%.2f;%.2f;%d;%d;%d;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f\n",
+                            mainframe.getMacAdress(),
+                            mainframe.getTimestamp(),
+                            mainframe.getIdentificaoMainframe(),
+                            mainframe.getUsoCpuTotal(),
+                            mainframe.getUsoRamTotal(),
+                            mainframe.getUsoDiscoTotal(),
+                            mainframe.getDiscoThroughputMbs(),
+                            mainframe.getDiscoIopsTotal().intValue(),
+                            mainframe.getDiscoReadCount(),
+                            mainframe.getDiscoWriteCount(),
+                            mainframe.getDiscoLatenciaMs(),
+                            processo.getNome1(), processo.getCpu1(), processo.getMem1(),
+                            processo.getNome2(), processo.getCpu2(), processo.getMem2(),
+                            processo.getNome3(), processo.getCpu3(), processo.getMem3(),
+                            processo.getNome4(), processo.getCpu4(), processo.getMem4(),
+                            processo.getNome5(), processo.getCpu5(), processo.getMem5(),
+                            processo.getNome6(), processo.getCpu6(), processo.getMem6(),
+                            processo.getNome7(), processo.getCpu7(), processo.getMem7(),
+                            processo.getNome8(), processo.getCpu8(), processo.getMem8(),
+                            processo.getNome9(), processo.getCpu9(), processo.getMem9()
+                    ));
+                }
+
+            }
         }
 
         return sb.toString();
